57c38436a9b6db3a9cd9c386b7dd15b8a2fb0a61
Kossych <81522445+Kossych@users.noreply.github.com>
2021-06-22 18:56:26 +0700

    Update Encode.cpp

--- Encode.cpp
+++ Encode.cpp
@@ -36,67 +36,71 @@ int main(){
     int count=0;
     char s; 
 
-    while(fin.get(s)){
-
+    //cout<<h0<<endl<<First_qtr<<endl<<Half<<endl<<Thride_qtr<<endl;
+    while(!fin.eof()){
+        fin.get(s);
         for(it=t.begin(); it->c!=s; it++);  
-        int d=h0-l0+1;
-        h0 = l0 + it->R * d/delitel-1;
-		l0 = l0 + it->L * d/delitel;
-
+        int d=h0-l0;
+        h0 = l0 + it->R * d/delitel;
+	l0 = l0 + it->L * d/delitel;
+        //cout<<it->c<<" : "<<l0<<" - "<<h0<<endl;
         for(;;){
             if(h0<Half){
                 count++;
-				if (count == 8)
-				{
-					fout.put(tmp);
-					tmp = 0;
-					count = 0;
-				}
-				for (; bits_to_follow > 0; bits_to_follow--,count++)
-				{
-					tmp = tmp | (1 << (7 - count));
-					if (count == 8)
-					{
-						fout.put(tmp);
-						tmp = 0;
-						count = 0;
-					}
-				}
+		if (count == 8)
+		{
+			fout.put(tmp);
+			tmp = 0;
+			count = 0;
+		}
+		for (; bits_to_follow > 0; bits_to_follow--)
+		{
+			tmp = tmp | (1 << (7 - count));
+	    	count++;
+		if (count == 8)
+		{
+			fout.put(tmp);
+			tmp = 0;
+			count = 0;
+		}
+	}
             }
             else if(l0>=Half){
                 tmp = tmp | (1 << (7 - count));
-				count++;
-				if (count == 8)
-				{
-					fout.put(tmp);
-					tmp = 0;
-					count = 0;
-				}
-				for (; bits_to_follow > 0; bits_to_follow--, count++)
-				{
-					if (count == 8)
-					{
-						fout.put(tmp);
-						tmp = 0;
-						count = 0;
-					}
-				}
+		count++;
+		if (count == 8)
+		{
+			fout.put(tmp);
+			tmp = 0;
+			count = 0;
+		}
+		for (; bits_to_follow > 0; bits_to_follow--)
+		{
+			count++;
+			if (count == 8)
+			{
+				fout.put(tmp);
+				tmp = 0;
+				count = 0;
+			}
+		}
                 l0-= Half; h0-= Half;
 
             }
             else if((l0>=First_qtr)&&(h0<Thride_qtr)){
                 bits_to_follow++;
-				l0 -= First_qtr; h0 -= First_qtr;
+		l0 -= First_qtr; h0 -= First_qtr;
             }
             else break;
-            cout<<it->c<<" : "<<l0<<" - "<<h0<<endl;
+
             l0 <<= 1;
-			h0 <<= 1;
+		h0 <<= 1;
             h0++;
+           // cout<<it->c<<" : "<<l0<<" - "<<h0<<endl;
         }
     }
-
-
+    
+ 
     fin.close();
     fout.close();
 }
