a7b4855c0d0da2b54c773f65bf1696d47fabf9dc
Kossych <81522445+Kossych@users.noreply.github.com>
2021-06-26 02:08:45 +0700

    Update Encode.cpp

--- Encode.cpp
+++ Encode.cpp
@@ -1,6 +1,6 @@
 #include "AC.h"
 
-
+unsigned short h0=h, l0=l;
 
 int main(){
     ifstream fin("Test.txt", ios::binary);
@@ -27,74 +27,68 @@ int main(){
     }
 
     list<Range>::iterator it;
-    int l0=0, h0=65535, i=0, delitel=t.back().R;
-    int First_qtr = (h0 + 1) / 4;   //16384
-	int Half = First_qtr * 2;   //32768
-	int Thride_qtr = First_qtr * 3; //49152
-	int bits_to_follow = 0; //сколько битов сбрасывать
+    
+    int delitel=t.back().R;
+	int bits_to_follow = 0;
 	char tmp = 0;
     int count=0;
     char s; 
-
-    //cout<<h0<<endl<<First_qtr<<endl<<Half<<endl<<Thride_qtr<<endl;
-    while(!fin.eof()){
-        fin.get(s);
+    while(fin.get(s)){  
         for(it=t.begin(); it->c!=s; it++);  
-        int d=h0-l0;
-        h0 = l0 + it->R * d/delitel;
-	l0 = l0 + it->L * d/delitel;
-        //cout<<it->c<<" : "<<l0<<" - "<<h0<<endl;
+        int d=h0-l0+1;
+        h0 = l0 + it->R * d/delitel-1;
+		l0 = l0 + it->L * d/delitel;
         for(;;){
             if(h0<Half){
                 count++;
-		if (count == 8)
-		{
-			fout.put(tmp);
-			tmp = 0;
-			count = 0;
-		}
-		for (; bits_to_follow > 0; bits_to_follow--)
-		{
-			tmp = tmp | (1 << (7 - count));
-	    	count++;
-		if (count == 8)
-		{
-			fout.put(tmp);
-			tmp = 0;
-			count = 0;
-		}
-	}
+				if (count == 8)
+				{
+					fout.put(tmp);
+					tmp = 0;
+					count = 0;
+				}
+				for (; bits_to_follow > 0; bits_to_follow--)
+				{
+					tmp = tmp | (1 << (7 - count));
+                    count++;
+					if (count == 8)
+					{
+						fout.put(tmp);
+						tmp = 0;
+						count = 0;
+					}
+				}
             }
             else if(l0>=Half){
                 tmp = tmp | (1 << (7 - count));
-		count++;
-		if (count == 8)
-		{
-			fout.put(tmp);
-			tmp = 0;
-			count = 0;
-		}
-		for (; bits_to_follow > 0; bits_to_follow--)
-		{
-			count++;
-			if (count == 8)
-			{
-				fout.put(tmp);
-				tmp = 0;
-				count = 0;
-			}
-		}
+				count++;
+				if (count == 8)
+				{
+					fout.put(tmp);
+					tmp = 0;
+					count = 0;
+				}
+				for (; bits_to_follow > 0; bits_to_follow--)
+				{
+                    count++;
+					if (count == 8)
+					{
+						fout.put(tmp);
+						tmp = 0;
+						count = 0;
+					}
+				}
                 l0-= Half; h0-= Half;
 
             }
             else if((l0>=First_qtr)&&(h0<Thride_qtr)){
                 bits_to_follow++;
-		l0 -= First_qtr; h0 -= First_qtr;
+				l0 -= First_qtr; h0 -= First_qtr;
             }
             else break;
 
             l0 <<= 1;
-		h0 <<= 1;
+			h0 <<= 1;
             h0++;
            // cout<<it->c<<" : "<<l0<<" - "<<h0<<endl;
         }
@@ -104,4 +98,6 @@ int main(){
     fin.close();
     fout.close();
 }
+
+
 
